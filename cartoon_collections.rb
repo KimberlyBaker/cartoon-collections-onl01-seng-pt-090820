aaed392de4e5d2d11d86ed22f3670450ea9cfad4
Kimberly Baker <bakerk2010@gmail.com>
2020-09-21 03:11:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,10 +16,12 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  
+  array.any? do |word|
+    word.length > 4
+  end
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
